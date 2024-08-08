3f185be1e35a91551b12d46c076a5ed2e2031545
yuma <yamano.rock.1202@gmail.com>
2024-08-08 23:00:07 +0900

    色を変更するようにしました

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ $("#addTodo").click(function () {
   $("input").val("");
 });
 
-$(document).on("change","input[type=checkbox]",function() {
-  if($(this).is(":checked")) {
-    $(this).parent().css("text-decoration","line-through");
+$(document).on("change", "input[type=checkbox]", function () {
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+    $(this).parent().css("color", "#CCC");
   } else {
-    $(this).parent().css("text-decoration","none");
+    $(this).parent().css("text-decoration", "none");
+    $(this).parent().css("color", "#000");
   }
-})
+});
